fc886a33eb6f3cdc8df055d56dfffc8274ecd9fc
EC2 Default User <ec2-user@ip-172-31-88-15.ec2.internal>
2020-05-23 12:21:15 +0000

    Implement advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,14 +4,14 @@ class SessionsController < ApplicationController
   
   def create
     # form_forから送られてきたemailを持つユーザーを探してuserに代入
-     user = User.find_by(email: params[:session][:email].downcase)
+     @user = User.find_by(email: params[:session][:email].downcase)
     # userか存在して、userのパスワードが送られてきたものと一致するか
-    if user && user.authenticate(params[:session][:password])
+    if @user && @user.authenticate(params[:session][:password])
       # ユーザーログイン後にユーザー情報のページにリダイレクトする
-      log_in user
+      log_in @user
       # ユーザーログイン後に永続セッション用の記憶トークンをcookieとデータベースに保存
-      remember user
-      redirect_to user
+      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+      redirect_to @user
     else
       # エラーメッセージを作成する
       flash.now[:danger] = 'Invalid email/password combination' 
